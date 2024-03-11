a2d152fe4bd339f089e79cb42fa7190f25a4b948
tanerjn <stanermetin@gmail.com>
2024-03-11 16:33:34 +0100

    build: added initial components for build automation

---
+++ src/utils/AppConfig.ts
@@ -0,0 +1,9 @@
+// FIXME: Update this configuration file based on your project information
+
+export const AppConfig = {
+  site_name: 'Starter',
+  title: 'Butik Sevdik Ecommerce Template',
+  description:
+    'Butik Sevdik Ecommerce Template in NextJS, React, HTML and TailwindCSS',
+  locale: 'en',
+};
